08db813338f874dc2bdbefc66dd066c6a021dae2
gowtham2k2 <gowthamangowda7@gmail.com>
2024-06-05 02:49:01 +0530

    feat: search list added

--- index.js
+++ index.js
@@ -10,20 +10,37 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  res.render("index.ejs", { response: "Response shows here" });
+  res.render("index.ejs", { response: "assets/videos/anime-collection.mp4" });
 });
 
 app.post("/search", async (req, res) => {
-  const animeName = req.body.animeName;
-  console.log(animeName);
+  const currentPage = req.body.page;
+  const animeSearch = req.body.animeSearch;
+  if (animeSearch !== null && animeSearch !== "") {
+    console.log(animeSearch);
+    try {
+      const response = await axios.get(
+        API_URL + `/anime?q=${animeSearch}&page=${currentPage}`
+      );
+      res.render("index.ejs", {
+        result: response.data,
+      });
+    } catch (err) {
+      res.render("index.ejs", { alert: err.message });
+    }
+  } else {
+    res.render("index.ejs", { alert: "Please enter the Anime title!" });
+  }
+});
+
+app.post("/anime", async (req, res) => {
+  const animeId = req.body.animeId;
   try {
-    const response = await axios.get(API_URL + `/anime?q=${animeName}`);
+    const response = await axios.get(API_URL + `/anime/${animeId}`);
     let info = JSON.stringify(response.data);
     fs.truncateSync("./anifo.json");
     fs.writeFileSync("./anifo.json", info, "utf-8");
-    res.render("index.ejs", {
-      result: response.data,
-    });
+    res.render("index.ejs", { content: response.data });
   } catch (err) {
     res.render("index.ejs", { alert: err.message });
   }
